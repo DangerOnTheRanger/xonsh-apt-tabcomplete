cd3362d5caeabbca4ff739ac4d8aa39f6b99f163
DangerOnTheRanger <tuxfreak@tuxfamily.org>
2016-06-16 00:50:01 -0500

    added more metadata to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,19 @@ setup(
     name="xonsh-apt-tabcomplete",
     version="0.1",
     license="BSD",
+    description="APT tabcomplete support for the Xonsh shell",
+    author="Kermit Alexander II",
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
     package_data={'xontrib': ['*.xsh']},
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: Plugins",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python"
+    ]
 )
